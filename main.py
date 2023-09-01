a0438bec40698a5cd98d2ff91cd90f5bea52d6a4
Carter McBroom <carter.mcbroom@instil.co>
2023-09-01 11:36:11 +0100

    image captioning example using dog

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
+from transformers import pipeline
+
+image_captioning_pipeline = pipeline("image-to-text", model="Salesforce/blip-image-captioning-base")
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+def caption_image(image_url):
+    output = image_captioning_pipeline(image_url)
+    print(output[0]['generated_text'])
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    image = "https://free-images.com/lg/ee7b/cow_animal_cow_head.jpg"
+    caption_image(image)
+
+# You can use this model for conditional and un-conditional image captioning ?
+
+# https://huggingface.co/Salesforce/blip-image-captioning-base
+
 
